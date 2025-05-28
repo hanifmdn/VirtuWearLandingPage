bcd2c3dff79a250381d95bc58ac04374c5d18ace
hanifmdn <hanifahmadnfl12@gmail.com>
2025-05-28 07:00:25 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-indigo-500 to-purple-600">
+      <h1 className="text-4xl font-bold text-white">Hello VirtuWear!</h1>
+    </div>
+  );
+}
+
+export default App;
